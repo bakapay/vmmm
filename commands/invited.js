2be69340d3721a95dec4c3449ba32fe5b4349bdc
zapayyyy <36130036+zapayyyy@users.noreply.github.com>
2018-06-07 14:41:00 +0300

    Update invited.js

--- commands/invited.js
+++ commands/invited.js
@@ -5,8 +5,17 @@ let member = message.mentions.members.first();
 
 if(!member) {
 message.guild.fetchInvites().then(invites => {
-	const invite = invites.find(inviter => inviter.inviter.id === message.author.id);
+	const invite = invites.find(inviter => inviter.inviter.id === message.author.id)
+	
+const k = new Discord.RichEmbed()
+.setAuthor("Invited | VermillionFamily", message.guild.iconURL)
+.setDescription("Nu ai invitat niciun utilizator pe server.")
+.setTimestamp()
+.setFooter(message.author.username)
 
+message.channel.send(k)
+if(!invite.maxUses) return message.channel.send(k)
+	
 const embed = new Discord.RichEmbed()
 .setAuthor("Invited | VermillionFamily", message.guild.iconURL)
 .setDescription("Ai invitat **" + invite.maxUses + "** utilizatori pe server.")
@@ -19,14 +28,21 @@ message.channel.send(embed)
 } else {
 	message.guild.fetchInvites().then(invites => {
 	const invite = invites.find(inviter => inviter.inviter.id === member.user.id);
-
-const embed = new Discord.RichEmbed()
+		
+const k2 = new Discord.RichEmbed()
+.setAuthor("Invited | VermillionFamily", message.guild.iconURL)
+.setDescription(member.user.username + " nu a invitat niciun utilizator pe server.")
+.setTimestamp()
+.setFooter(member.user.username)		
+if(!invite.maxUses) return message.channel.send(k2)
+		
+const embed2 = new Discord.RichEmbed()
 .setAuthor("Invited | VermillionFamily", message.guild.iconURL)
 .setDescription(member.user.username + " a invitat **" + invite.maxUses + "** utilizatori pe server.")
 .setTimestamp()
 .setFooter(member.user.username)
 
-message.channel.send(embed)
+message.channel.send(embed2)
 
 })
 }
